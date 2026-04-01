05d91611d61cde654c49541170bbc75ef030577e
lsy3709 <lsy3709@naver.com>
2026-04-01 14:09:19 +0900

    5교시, 미니 프로젝트 화면 작업중, SplashScreen, main.dart 작업 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
-
-import 'package:busanit_401_k9_flutter_project/screen/basic1/_8_MyAppClass.dart';
-import 'package:busanit_401_k9_flutter_project/screen/basic1/_9_MyAppTest2.dart';
+import 'package:busanit_401_k9_flutter_project/screen/basic2-miniproject/SplashScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  // 1. main 함수는 단순히 MyApp이라는 위젯을 실행하는 역할만 합니다.
-  runApp(MyAppTest());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+      routes: {
+        // '/main':    (context) => const MainScreen(),
+        // '/signup':  (context) => const SignupScreen(),
+        // '/login':   (context) => const LoginScreen(),
+        // '/details': (context) => const DetailsScreen(),
+      },
+    );
+  }
 }
 
